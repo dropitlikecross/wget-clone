1b3fa1d3b31643a862748dab926bfabb358797e2
Cross <44788043+dropitlikecross@users.noreply.github.com>
2020-10-25 03:02:47 +0900

    Create cloner.sh

---
+++ cloner.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+#This script clones a website
+
+$ wget \
+     --recursive \
+     --no-clobber \
+     --page-requisites \
+     --html-extension \
+     --convert-links \
+     --restrict-file-names=windows \
+     --domains website.org \
+     --no-parent \
+        https://ownthe.cloud/
