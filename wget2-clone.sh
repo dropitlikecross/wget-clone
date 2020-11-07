6c60b166eb662caff6bdc4b365961542de37c7af
Cross <44788043+dropitlikecross@users.noreply.github.com>
2020-11-08 00:47:00 +0900

    Create wget2-clone.sh

---
+++ wget2-clone.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+wget2
+--no-clobber \
+--mirror \
+--page-requisites \
+--adjust-extension \
+--no-parent \
+--convert-links \
+--reject '*.js,*.css,*.ico,*.iso' \
+--ignore-tags=script \
+--execute robots=off \
+--user-agent=Mozilla \
+--max-threads 5 \
+--continue \
+www.example.com
